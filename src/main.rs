ffdb626144544459b077954658e216a55ccde9b0
Ellen <supbscripter@gmail.com>
2022-09-18 06:36:54 +0100

    tile kinds

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,21 @@ pub mod hexmap;
 #[derive(Component)]
 struct MyTileData {
     render: Entity,
+    kind: TileKind,
+}
+
+enum TileKind {
+    Ground,
+    Wall,
+}
+
+impl TileKind {
+    pub fn color(&self) -> Color {
+        match self {
+            Self::Ground => Color::BEIGE,
+            Self::Wall => Color::DARK_GRAY,
+        }
+    }
 }
 
 #[derive(Component)]
@@ -34,11 +49,21 @@ fn init_map(mut cmds: Commands<'_, '_>) {
     let map = HexMap::new(
         10,
         10,
-        std::iter::from_fn(|| {
-            Some(MyTileData {
-                render: cmds.spawn_bundle((RenderTileEntity,)).id(),
+        {
+            let mut i = 0;
+            let cmds = &mut cmds;
+            std::iter::from_fn(move || {
+                i += 1;
+                Some(MyTileData {
+                    render: cmds.spawn_bundle((RenderTileEntity,)).id(),
+                    kind: if i % 6 == 0 {
+                        TileKind::Wall
+                    } else {
+                        TileKind::Ground
+                    },
+                })
             })
-        })
+        }
         .take(10 * 10),
     );
     cmds.insert_resource(map);
@@ -48,11 +73,13 @@ fn update_hexmap_render(mut cmds: Commands<'_, '_>, map: Res<HexMap<MyTileData>>
     let mut hashmap = HashSet::new();
     for q in 0..10 {
         for r in 0..10 {
-            let e = map.get(HexPos { q, r }).render;
+            let tile = map.get(HexPos { q, r });
+            let e = tile.render;
             assert_eq!(hashmap.contains(&e), false);
             hashmap.insert(e);
             cmds.entity(e).insert_bundle(SpriteBundle {
                 sprite: Sprite {
+                    color: tile.kind.color(),
                     custom_size: Some(vec2(30., 30.)),
                     ..default()
                 },
